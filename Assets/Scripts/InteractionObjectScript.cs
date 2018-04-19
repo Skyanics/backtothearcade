88072083c585a48c0c81935c8b207a44c93aeb0f
Sander (HomePC) <sanderwinkeler2@gmail.com>
2018-04-19 23:03:29 +0200

    A Player Update
    ~ Added walljump & interaction

---
+++ Assets/Scripts/InteractionObjectScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObjectScript : MonoBehaviour {
+
+    public bool isPushPullObject;
+    public bool isNear;
+    public GameObject player;
+    
+	
+	// Update is called once per frame
+	void Update () {
+       
+
+        //PUSH OR PULL OBJECT
+        if (isPushPullObject == true && isNear == true)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                transform.SetParent(player.transform);
+                player.GetComponent<Player>().speed = 1.0f;
+                player.GetComponent<Player>().jumpForce = 0.0f;
+                player.GetComponent<Player>().isPulling = true;
+                GetComponent<Rigidbody>().isKinematic = true;
+                
+            
+                
+            }
+
+            else
+            {
+                transform.SetParent(null);
+                player.GetComponent<Player>().speed = 5.0f;
+                player.GetComponent<Player>().jumpForce = 10.0f;
+                GetComponent<Rigidbody>().isKinematic = false;
+                player.GetComponent<Player>().isPulling = false;
+            }
+        }
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isNear = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isNear = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody rig;
+    private CharacterController controller;
+    public float speed = 5.0f;
+    private float verticalVelocity;
+    public float Gravity;
+    public float jumpForce = 10.0f;
+    public bool isPulling;
+
+    private Quaternion rot;
+    private Vector3 moveVector;
+    private Vector3 lastMove;
+
+    // Use this for initialization
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector = Vector3.zero;
+        moveVector.x = Input.GetAxis("Horizontal") * speed;
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -1;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+
+        else
+        {
+            verticalVelocity -= Gravity * Time.deltaTime;
+            moveVector = lastMove;
+        }
+
+        moveVector.y = 0;
+        moveVector.Normalize();
+        moveVector *= speed;
+        moveVector.y = verticalVelocity;
+
+        controller.Move(moveVector * Time.deltaTime);
+        lastMove = moveVector;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rot = Quaternion.LookRotation(Vector3.left);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rot = Quaternion.LookRotation(Vector3.right);
+        }
+
+        if (!isPulling)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (!controller.isGrounded && hit.normal.y < 0.1f)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.DrawRay(hit.point, hit.normal, Color.red, 1.25f);
+                verticalVelocity = jumpForce;
+                moveVector = hit.normal * speed;
+                rot = Quaternion.LookRotation(hit.normal);
+            }
+        }
+
+    }
+}
