10f639ecd8fd45e1349229cee010da25031961e1
Sander (HomePC) <sanderwinkeler2@gmail.com>
2018-04-22 14:51:15 +0200

    Enemies Ahoy
    - Added temporary Rat model
    - Added Loading Screen
    - Added Title Screen
    - Added Title Screen Animation
    - Added Enemy AI

---
+++ Assets/LoadingScreen.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LoadingScreen : MonoBehaviour
+{
+
+    AsyncOperation ao;
+    public Image progBar;
+
+    public bool isFakeLoadingBar = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        LoadLevel();
+    }
+
+    public void LoadLevel()
+    {
+ 
+
+        if (!isFakeLoadingBar)
+        {
+            StartCoroutine(LoadLevelWithRealProgress());
+        }
+    }
+
+    IEnumerator LoadLevelWithRealProgress()
+    {
+        yield return new WaitForSeconds(1);
+
+        ao = SceneManager.LoadSceneAsync(2);
+
+        while (!ao.isDone)
+        {
+            progBar.fillAmount = ao.progress;
+
+            Debug.Log(ao.progress);
+            yield return null;
+        }
+    }
+}
+
+
