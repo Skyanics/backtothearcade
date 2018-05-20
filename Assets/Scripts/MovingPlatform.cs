5937898301299d181b854d986fa44bf0d3adff88
Sander (HomePC) <sanderwinkeler2@gmail.com>
2018-05-20 21:29:11 +0200

    Is it just me or are we defying physics?
    - Added moving platforms
    - Added Crystals

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    public GameObject player;
+    public Transform farEnd;
+    private Vector3 frometh;
+    private Vector3 untoeth;
+    public float secondsForOneLength = 20f;
+
+    void Start()
+    {
+        frometh = transform.position;
+        untoeth = farEnd.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(frometh, untoeth,
+         Mathf.SmoothStep(0f, 1f,
+          Mathf.PingPong(Time.time / secondsForOneLength, 1f)
+        ));
+    }
+
+    void OnTriggerEnter()
+    {
+        player.transform.SetParent(this.transform);
+    }
+
+    void OnTriggerExit()
+    {
+        player.transform.SetParent(null);
+    }
+}
